f02e2bbc98b908e08b1493fede307e8638479213
bcs44 <bruna_silva_05@hotmail.com>
2019-03-07 09:42:35 +0000

    Dev

--- pubic/apidoc/api_project.js
+++ pubic/apidoc/api_project.js
@@ -1,13 +1,13 @@
 define({
   "name": "bruna",
   "version": "0.1.1",
-  "description": "",
+  "description": "node app ",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-02-28T17:18:10.890Z",
+    "time": "2019-03-06T17:56:36.588Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const doctorRoutes = require('./doctors');
 const patientsRoutes = require('./patients');
 const consultRoutes = require('./consultations');
 
+//apidoc -f  /routes -o pubic/apidoc
+
 module.exports = function(app, db) {
 	doctorRoutes(app, db);
 	patientsRoutes(app, db);
